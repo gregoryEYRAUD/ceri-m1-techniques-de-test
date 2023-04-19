8b0db70149a454d5df5d60601a1d05e2082df566
eyraud <gregory.eyraud@alumni.univ-avignon.fr>
2023-04-19 12:49:01 +0200

    Implémentation Pokedex

--- src/main/java/fr/univavignon/pokedex/api/Pokedex.java
+++ src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -5,42 +5,65 @@ import fr.univavignon.pokedex.api.PokedexException;
 import fr.univavignon.pokedex.api.Pokemon;
 import fr.univavignon.pokedex.api.PokemonMetadata;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class Pokedex implements IPokedex {
+
+    private ArrayList<Pokemon> pokemons;
+
+    public Pokedex() {
+        pokemons = new ArrayList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return pokemons.size();
     }
 
     @Override
     public int addPokemon(Pokemon pokemon) {
-        return 0;
+        pokemons.set(pokemon.getIndex(), pokemon);
+        return pokemon.getIndex();
     }
 
     @Override
     public Pokemon getPokemon(int id) throws PokedexException {
-        return null;
+        try{
+            return pokemons.get(id);
+        } catch (Exception e) {
+            throw new PokedexException(e.getMessage());
+        }
+
     }
 
     @Override
     public List<Pokemon> getPokemons() {
-        return null;
+        return pokemons;
     }
 
     @Override
     public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
-        return null;
+        ArrayList<Pokemon> list = pokemons;
+        list.sort(order);
+        return list;
     }
 
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-        return null;
+        PokemonMetadata metadata;
+        try {
+             metadata = getPokemonMetadata(index);
+             } catch (PokedexException e) {
+                throw new RuntimeException(e);
+        }
+        return new Pokemon(index, metadata.getName(), metadata.getAttack(), metadata.getDefense(), metadata.getStamina(), cp, hp, dust, candy, 53);
     }
 
     @Override
     public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-        return null;
+        Pokemon pokemon = pokemons.get(index);
+        return new PokemonMetadata(index, pokemon.getName(), pokemon.getAttack(), pokemon.getDefense(), pokemon.getStamina());
     }
 }
